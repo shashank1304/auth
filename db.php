b34d288ac91e7936f52984ab28e5ebae319865ec
shashank1304 <shashankkorram@gmail.com>
2025-02-27 12:36:34 +0530

    changed dp

--- db.php
+++ db.php
@@ -12,15 +12,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Add email field to the registration process
-$email = $_POST['email'];
-
-// Insert user data into the database
-$sql = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
 ?>
